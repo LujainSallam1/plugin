31f41fb315ef5987e16076cea773e6d1b5faf5cc
lujainsallam1 <lujainsallam1@gmail.com>
2023-11-13 16:42:48 +0100

    refresh token

--- ApiDataSender.js
+++ ApiDataSender.js
@@ -21,8 +21,14 @@ const nameIdPolicy_input = document.getElementById("nameIdPolicy");
 const SignatureAlgorithm_input=document.getElementById("SignatureAlgorithm");
 const SAMLSignatureKeyName_input=document.getElementById("SAMLSignatureKeyName");
 const ValidatingX509Certificates_input=document.getElementById("ValidatingX509Certificates");
-
+let newAccessToken;
 buttonInput.addEventListener('click', () => {
+    keycloak.updateToken(180).then((bool) => {
+    if (bool) {
+      console.log("Token is updated");
+      newAccessToken = keycloak.token;
+
+      // بعد ذلك يُمكنك استخدام الـ Access Token لإرسال الطلب HTTP
     
   
     const authnContextClassRefs=[]
@@ -55,68 +61,110 @@ buttonInput.addEventListener('click', () => {
     var SignatureAlgorithm=SignatureAlgorithm_input.value;
     var SAMLSignatureKeyName=SAMLSignatureKeyName_input.value;
     const  ValidatingX509Certificates= ValidatingX509Certificates_input.value;
-    const url = `http://localhost:3000/api`;
+    const url = `http://localhost:8080/admin/realms/master/identity-provider/instances`;
 
-    const data = {
-        alias: SamlExtended,
-        displayName: Display_Name,
-        addReadTokenRoleOnCreate: storedTokensReadable_value,
-        trustEmail: trustEmail_value,
-        linkOnly: accountLinkingOnly_value,
-        postBrokerLoginFlowAlias: postLoginFlow,
-        firstBrokerLoginFlowAlias: firstLoginFlow,
-
+   const data = {
+        "alias": SamlExtended,
+        "displayName": Display_Name,
+        "internalId": "a3e9b939-357f-4bff-bac6-8225aec4a9e4",
+        "providerId": "saml-extended",
+        "enabled": "true",
+        "updateProfileFirstLoginMode": "on",
+        "trustEmail": trustEmail_value,
+        "storeToken": storeToken_value,
+        "addReadTokenRoleOnCreate": storedTokensReadable_value,
+        "authenticateByDefault": "false",
+        "linkOnly": accountLinkingOnly_value,
+        "firstBrokerLoginFlowAlias": firstLoginFlow,
+        "postBrokerLoginFlowAlias": postLoginFlow,
         config: {
-            guiOrder: Display_Order,
-            singleLogoutServiceUrl: Single_Logout_Service_URL,
-            idpEntityId: Identity_Provider_Entity_ID,
-            singleSignOnServiceUrl: Single_Sign_On_Service_URL,
-            entityId: Service_Provider_Entity_ID,
-            backchannelSupported: backchannel_value,
-            allowCreate: allowCreat_value,
-            postBindingResponse: httpPostBindingResponse_value,
-            postBindingLogout: httpPostBindingLogout_value,
-            wantAssertionsSigned: wantAssertionsSigned_value,
-            wantAssertionsEncrypted: wantAssertionsEncrypted_value,
-            forceAuthn: forceAuthentication_value,
-            signSpMetadata: signMetadata_value,
-            loginHint: passSubject_value,
-            storeToken: storeToken_value,
-            hideOnLoginPage: hideLoginPage_value,
-            allowedClockSkew: allowedClockSkew,
-            attributeConsumingServiceIndex: attributeConsumingServiceIndex,
-            attributeConsumingServiceName: attributeConsumingServiceName,
-            authnContextClassRefs: authnContextClassRefs ,
-            authnContextDeclRefs: authnContextDeclRefs,
-            authnContextComparisonType: comparison,
-            syncMode: syncMode,
-            principalType: principalType,
-            nameIDPolicyFormat: nameIdPolicy1,
-            signatureAlgorithm:SignatureAlgorithm,
-            xmlSigKeyInfoKeyNameTransformer:SAMLSignatureKeyName,
-            wantAuthnRequestsSigned:wantAuthnRequestsSigned_value,
-            validateSignature:validateSignatures_value,
-            signingCertificate:ValidatingX509Certificates
-          
-            
+            "postBindingLogout": httpPostBindingLogout_value,
+            "postBindingResponse": httpPostBindingResponse_value,
+            "singleLogoutServiceUrl": Single_Logout_Service_URL,
+            "backchannelSupported": backchannel_value,
+            "xmlSigKeyInfoKeyNameTransformer":SAMLSignatureKeyName,
+            "idpEntityId": Identity_Provider_Entity_ID,
+            "loginHint": passSubject_value,
+            "allowCreate": allowCreat_value,
+            "authnContextComparisonType": comparison,
+            "syncMode": syncMode,
+            "singleSignOnServiceUrl": Single_Sign_On_Service_URL,
+            "wantAuthnRequestsSigned": "true",
+            "allowedClockSkew": allowedClockSkew,
+            "guiOrder": Display_Order,
+            "validateSignature":validateSignatures_value,
+            "hideOnLoginPage": hideLoginPage_value,
+            "signingCertificate":ValidatingX509Certificates,
+            "nameIDPolicyFormat": nameIdPolicy1,
+            "entityId": Service_Provider_Entity_ID,
+            "attributeConsumingServiceName": attributeConsumingServiceName,
+            "signSpMetadata": signMetadata_value,
+            "wantAssertionsEncrypted": wantAssertionsEncrypted_value,
+            "signatureAlgorithm":SignatureAlgorithm,
+            "wantAssertionsSigned": wantAssertionsSigned_value,
+            "postBindingAuthnRequest":wantAuthnRequestsSigned_value,
+            "forceAuthn": forceAuthentication_value,
+            "attributeConsumingServiceIndex": attributeConsumingServiceIndex,
+            "principalType": principalType,
         }
     };
+    function removeEmptyStrings(obj) {
+        for (const key in obj) {
+            if (typeof obj[key] === 'string' && obj[key].trim() === "") {
+                delete obj[key];
+            } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+                removeEmptyStrings(obj[key]);
+            }
+        }
+    }
 
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ data: data })
-    })
-        .then(response => response.json())
-        .then(data => {
-            `displayData.textContent = Data received: ${JSON.stringify(data)}`;
+    removeEmptyStrings(data);
 
-        })
-        .catch(error => {
-            console.error('error', error);
-        });
-        document.getElementById("ValidatingX509Certificates").value='';
+    // التحقق من وجود قيم في الحقول داخل config
+    const configKeys = Object.keys(data.config);
+    for (const key of configKeys) {
+        if (typeof data.config[key] === 'string' && data.config[key].trim() === "") {
+            delete data.config[key];
+        }
+    }
+    if (Array.isArray(data.config.authnContextClassRefs) && data.config.authnContextClassRefs.length === 0) {
+        delete data.config.authnContextClassRefs;
+    }
+
+    if (Array.isArray(data.config.authnContextDeclRefs) && data.config.authnContextDeclRefs.length === 0) {
+        delete data.config.authnContextDeclRefs;
+    }
+
+    
+
+    // إرسال البيانات إلى الخادم
+console.log(data);
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${newAccessToken}`
+    },
+    body: JSON.stringify(data)
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.text();
+})
+.then(data => {
+    // Parse JSON if data is not empty, otherwise return an empty object
+    const parsedData = data ? JSON.parse(data) : {};
+    console.log(`Data received: ${JSON.stringify(parsedData)}`);
+})
+.catch(error => {
+    console.error('Error:', error);
 });
-
+document.getElementById("ValidatingX509Certificates").value = '';
+} else {
+    console.log("Token is not updated");
+}
+});
+    });
