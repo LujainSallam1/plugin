1e8bb490c1a9eda0855e49ee67dc3d9529e39d8c
lujainsallam1 <lujainsallam1@gmail.com>
2023-11-20 11:58:40 +0100

    de gebruiker kan de pagina helamaal niet zien als hij geen admin is

---
+++ js/keycloack.js
@@ -0,0 +1,55 @@
+let accessToken;
+const keycloak = Keycloak({
+    url: 'http://localhost:8080',
+    realm: 'master',
+    clientId: 'frontend',
+    redirectUri: 'http://localhost:3000/index.html',
+    enableDebug: true,
+    
+  });
+  
+  document.getElementById('login').addEventListener('click', () => {
+    keycloak.login();
+  });
+  
+  document.getElementById('logout').addEventListener('click', () => {
+     const clientid='frontend';
+     const postLogoutRedirect = 'http://localhost:3000/index.html';
+     window.location.href =`http://localhost:8080/realms/master/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
+  });
+  
+  keycloak
+    .init({ onLoad: 'login-required' })
+    .then((authenticated) => {
+      if (authenticated) {
+        accessToken = keycloak.token;
+        console.log(`Access Token: ${accessToken}`);
+  
+        // Check if the user has the "admin" role
+       const tokenParsed = keycloak.tokenParsed;
+        const roles = tokenParsed.realm_access.roles;
+  
+
+        if (roles.includes("admin")) {
+          console.log("User has 'admin' role");
+          document.body.style.display='block';
+        } else {
+          alert("User does not have admin role. Access denied.");
+             const clientid='frontend';
+             const postLogoutRedirect = 'http://localhost:3000/index.html';
+             window.location.href =`http://localhost:8080/realms/master/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
+         
+        }
+        console.log("Access Token:", keycloak.token);
+
+      }
+   else {
+
+        alert("User authentication failed!");
+       
+      }
+    })
+    .catch(() => {
+      alert("Could not authenticate the user!");
+    });
+  
--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 const express = require('express');
+
+const path = require('path');
+
+
 const app = express();
-const cors=require('cors')
 const port = 3000;
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
 
-app.use(cors());
-app.use(express.json());
+ const memoryStore = new session.MemoryStore();
 
-app.post('/api', (req, res) => {
-    const { data }=req.body;
-    // res.json({massage:`ok ${data}`})
-    console.log(data);
+ const kcConfig = {
+    clientId: 'frontend',
+    bearerOnly: true,
+    serverUrl: 'http://localhost:8080',
+    realm: 'master',
+    realmPublicKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoI9pBNHkOKuucvPM0ICkvAk/x01KblcgjrYAWXuo/atQebYxCl1vAc8ATrlVQ84w+HZMAG64KmV4vCVTOXOLb5dSO8LIl1KQKmQCDu2Tdjvo6b0Gchn8mtadeNaDfsFSFUByM2kiSpFdb5rqREhtgI5Z7WITlqmAKfPppbNz1ULqpzHY5tsYKrxDQ5Vd2fRA1WLERV0Gb2vYmJuSTzJ8fVX4MJ3VCaWlQWGuhvl7etkEain7jGA91eZ8zgByk08nfsbHwm7YvZWpk0sE+2ZbytxlP9YaC9wlysgA36wC0C+Z+PtU7a1bvb8K0s6eMIIs09GunUcX9CI2JywWWnMdzQIDAQAB'
+};
 
-     res.json(data);
-});
+const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
 
-app.listen(port, () => {
-    console.log(`server ${port}`);
-});
+
+app.use(session({ secret: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoI9pBNHkOKuucvPM0ICkvAk', resave: false, saveUninitialized: true }));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'pages')));
+
+app.use( keycloak.middleware() );
+
+function extraSpecialHandler(req, res) {
+    res.sendFile(path.join(__dirname, '/pages/index.html'));
+  }
+  
+  app.use(session({ secret: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoI9pBNHkOKuucvPM0ICkvAk', resave: false, saveUninitialized: true }));
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  app.use('/js', express.static(path.join(__dirname, 'js')));
+  app.use(express.static(path.join(__dirname, 'pages')));
+  
+  app.use(keycloak.middleware());
+  
+  // حماية مسار /index.html باستخدام دور "admin" واستخدام دالة extraSpecialHandler
+  // app.get('/index.html', keycloak.protect('admin'), (req, res) => {
+  //   console.log('User has access to /index.html');
+  //   extraSpecialHandler(req, res);
+  // });
+  
+
+  app.get('/index', (req, res) => {
+    console.log('User has access to /index');
+    res.sendFile(path.join(__dirname, '/pages/index.html'));
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
